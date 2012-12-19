aee4b92df70b5cc5076450124441aef54083635b
7shi <7shi@live.jp>
2012-12-19 18:52:20 +0900

    盤の中身を配列で管理

--- Reversi/Form1.cs
+++ Reversi/Form1.cs
@@ -11,12 +11,18 @@ namespace Reversi
 {
     public partial class Form1 : Form
     {
+        int[,] board = new int[8, 8];
+
         public Form1()
         {
             InitializeComponent();
             Text = "リバーシ";
             ClientSize = new Size(260, 260);
             BackColor = Color.Green;
+            board[3, 3] = 2;
+            board[4, 3] = 1;
+            board[3, 4] = 1;
+            board[4, 4] = 2;
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -27,10 +33,20 @@ namespace Reversi
                 e.Graphics.DrawLine(Pens.Black, i * 30 + 10, 10, i * 30 + 10, 250);
                 e.Graphics.DrawLine(Pens.Black, 10, i * 30 + 10, 250, i * 30 + 10);
             }
-            e.Graphics.FillEllipse(Brushes.White, 101, 101, 28, 28);
-            e.Graphics.FillEllipse(Brushes.Black, 131, 101, 28, 28);
-            e.Graphics.FillEllipse(Brushes.Black, 101, 131, 28, 28);
-            e.Graphics.FillEllipse(Brushes.White, 131, 131, 28, 28);
+            for (int y = 0; y <= 7; y++)
+            {
+                for (int x = 0; x <= 7; x++)
+                {
+                    if (board[x, y] == 1)
+                    {
+                        e.Graphics.FillEllipse(Brushes.Black, x * 30 + 11, y * 30 + 11, 28, 28);
+                    }
+                    else if (board[x, y] == 2)
+                    {
+                        e.Graphics.FillEllipse(Brushes.White, x * 30 + 11, y * 30 + 11, 28, 28);
+                    }
+                }
+            }
         }
     }
 }
