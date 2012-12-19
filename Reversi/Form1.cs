dcb8ebf464f1733a06d3c85f191b819d2c9f9342
7shi <7shi@live.jp>
2012-12-19 18:49:55 +0900

    図形描画テスト

--- Reversi/Form1.cs
+++ Reversi/Form1.cs
@@ -15,5 +15,14 @@ namespace Reversi
         {
             InitializeComponent();
         }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.DrawLine(Pens.Black, 10, 10, 20, 20);
+            e.Graphics.DrawRectangle(Pens.Black, 10, 10, 20, 20);
+            e.Graphics.DrawEllipse(Pens.Blue, 10, 10, 20, 20);
+            e.Graphics.FillRectangle(Brushes.Red, 40, 10, 20, 20);
+        }
     }
 }
