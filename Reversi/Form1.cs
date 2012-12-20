bd10cf343ca54bc9219132622e62601dfea7d3bc
7shi <7shi@live.jp>
2012-12-20 12:39:06 +0900

    文字を中央揃え

--- Reversi/Form1.cs
+++ Reversi/Form1.cs
@@ -41,13 +41,25 @@ namespace Reversi
                     DrawStone(e, board[x, y], x * 30 + 11, y * 30 + 11);
                 }
             }
-            e.Graphics.DrawString("Turn", Font, Brushes.White, 262, 235);
+            e.Graphics.DrawString("Turn", Font, Brushes.White,
+                new Rectangle(250, 230, 50, 30),
+                new StringFormat
+                {
+                    Alignment = StringAlignment.Center,
+                    LineAlignment = StringAlignment.Center
+                });
             DrawStone(e, player, 261, 201);
             if (message != "")
             {
                 e.Graphics.FillRectangle(Brushes.White, 20, 120, 220, 20);
                 e.Graphics.DrawRectangle(Pens.Red, 20, 120, 220, 20);
-                e.Graphics.DrawString(message, Font, Brushes.Black, 30, 125);
+                e.Graphics.DrawString(message, Font, Brushes.Black,
+                    new Rectangle(20, 120, 220, 20),
+                    new StringFormat
+                    {
+                        Alignment = StringAlignment.Center,
+                        LineAlignment = StringAlignment.Center
+                    });
             }
         }
 
